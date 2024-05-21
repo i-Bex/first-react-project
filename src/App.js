3d1aabd0eae153990eda04894c01fd76a1f855b1
i-Bex <i-Bex@users.noreply.github.com>
2024-05-21 22:02:47 +0800

    Add components and their functionality

--- src/App.js
+++ src/App.js
@@ -1,8 +1,70 @@
+import { useState, useEffect } from "react";
+
+function Quote({ adviceNumber, quoteText, errorMessage }) {
+  return (
+    <>
+      <p className="quote-container__advice-number">advice # {adviceNumber}</p>
+      <h2 className="quote-container__quote">
+        {quoteText ? `"${quoteText}"` : errorMessage ? "" : "Loading Quote..."}
+      </h2>
+      {errorMessage && (
+        <h2 className="quote-container__error-message">{errorMessage}</h2>
+      )}
+    </>
+  );
+}
+
+function GenerateQuoteBtn({ generateQuote }) {
+  return (
+    <>
+      <button
+        type="button"
+        className="quote-container__generate-quote-btn"
+        aria-label="generate advice"
+        onClick={generateQuote}
+      >
+        <img
+          src="../images/icon-dice.svg"
+          alt=""
+          height={24}
+          width={24}
+          aria-hidden="true"
+        ></img>
+      </button>
+    </>
+  );
+}
+
 function App() {
-  return (  
-    <div className="main-container">
-      <h1 className="main-heading">hello world! this is my main heading</h1>    
-    </div> 
+  const [quote, setQuote] = useState(null);
+  const [errorMessage, setErrorMessage] = useState(null);
+
+  const fetchQuote = async () => {
+    try {
+      setQuote(null);
+      setErrorMessage(null);
+      const quoteRequest = await fetch("https://api.adviceslip.com/advice");
+      const quoteResponse = await quoteRequest.json();
+      setQuote(quoteResponse);
+    } catch (error) {
+      setErrorMessage("Failed to Load Advice");
+    }
+  };
+
+  useEffect(() => {
+    fetchQuote();
+  }, []);
+
+  return (
+    <>
+      <Quote
+        adviceNumber={quote?.slip?.id}
+        quoteText={quote?.slip?.advice}
+        errorMessage={errorMessage}
+      ></Quote>
+      <div className="quote-container__separator"></div>
+      <GenerateQuoteBtn generateQuote={fetchQuote}></GenerateQuoteBtn>
+    </>
   );
 }
 
